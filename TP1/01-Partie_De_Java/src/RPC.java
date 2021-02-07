c3a4ff316593ab0b732e58d15b81ff4bd764e6b8
Mehran <mehran.git@hotmail.com>
2021-02-06 21:19:19 -0500

    modification print String

--- TP1/01-Partie_De_Java/src/RPC.java
+++ TP1/01-Partie_De_Java/src/RPC.java
@@ -9,33 +9,33 @@ public class RPC {
         int scoreDifference;
 
         // un message de bienvenue s'affiche
-        System.out.println("-----------------------------------------------\n--- Bienvenue au jeu de roche-papier-ciseau ---\n-----------------------------------------------");
+        Pep8.stro("-----------------------------------------------\n--- Bienvenue au jeu de roche-papier-ciseau ---\n-----------------------------------------------\n");
 
         // demande le nombre de manches qu'on souhaite jouer dans la partie
-        System.out.println("Combien de manches voulez-vous jouer ? ");
+        Pep8.stro("Combien de manches voulez-vous jouer ? ");
         manches = Pep8.deci();
         if (manches % 2 == 0) {
             manches++;
         }
         int counter = manches;
         do {
-            System.out.println("Il reste " + counter + " manche(s) à jouer.");
+            Pep8.stro("Il reste " + counter + " manche(s) à jouer.\n");
 
             //Verification l'entrer de Joueur 1 :
-            System.out.println("JOUEUR 1, quel est votre choix? [r/p/c]");
+            Pep8.stro("JOUEUR 1, quel est votre choix? [r/p/c]\n");
             Pep8.chari();
             joueurs1Choix = Pep8.chari();
             if (joueurs1Choix != 'r' && joueurs1Choix != 'p' && joueurs1Choix != 'c') {
-                System.out.println("Erreur d'entrée! Programme terminé.");
+                Pep8.stro("Erreur d'entrée! Programme terminé.");
                 Pep8.stop();
             }
 
             //Verification l'entrer de Joueur 2 :
-            System.out.println("JOUEUR 2, quel est votre choix? [r/p/c]");
+            Pep8.stro("JOUEUR 2, quel est votre choix? [r/p/c]\n");
             Pep8.chari();
             joueurs2Choix = Pep8.chari();
             if (joueurs2Choix != 'r' && joueurs2Choix != 'p' && joueurs2Choix != 'c') {
-                System.out.println("Erreur d'entrée! Programme terminé.");
+                Pep8.stro("Erreur d'entrée! Programme terminé.");
                 Pep8.stop();
             }
 
@@ -43,14 +43,14 @@ public class RPC {
             switch (joueurs1Choix) {
                 case 'r':
                     if (joueurs2Choix == 'r') {
-                        System.out.println("Manche null \n");
+                        Pep8.stro("Manche null \n\n");
                     }else if (joueurs2Choix == 'c') {
                         joueurs1Score++;
-                        System.out.println("JOUEUR 1 a gagné cette manche! Score:" + joueurs1Score + "-" + joueurs2Score + "\n");
+                        Pep8.stro("JOUEUR 1 a gagné cette manche! Score:" + joueurs1Score + "-" + joueurs2Score + "\n\n");
                         counter--;
                     }else{
                         joueurs2Score++;
-                        System.out.println("JOUEUR 2 a gagné cette manche! Score:" + joueurs1Score + "-" + joueurs2Score + "\n");
+                        Pep8.stro("JOUEUR 2 a gagné cette manche! Score:" + joueurs1Score + "-" + joueurs2Score + "\n\n");
                         counter--;
                     }
                     break;
@@ -58,13 +58,13 @@ public class RPC {
                 case 'c':
                     if (joueurs2Choix == 'r') {
                         joueurs2Score++;
-                        System.out.println("JOUEUR 2 a gagné cette manche! Score:" + joueurs1Score + "-" + joueurs2Score + "\n");
+                        Pep8.stro("JOUEUR 2 a gagné cette manche! Score:" + joueurs1Score + "-" + joueurs2Score + "\n\n");
                         counter--;
                     }else if (joueurs2Choix == 'c') {
-                        System.out.println("Manche null + \n");
+                        Pep8.stro("Manche null + \n\n");
                     }else{
                         joueurs1Score++;
-                        System.out.println("JOUEUR 1 a gagné cette manche! Score:" + joueurs1Score + "-" + joueurs2Score + "\n");
+                        Pep8.stro("JOUEUR 1 a gagné cette manche! Score:" + joueurs1Score + "-" + joueurs2Score + "\n\n");
                         counter--;
                     }
                     break;
@@ -72,14 +72,14 @@ public class RPC {
                 case 'p':
                     if (joueurs2Choix == 'r') {
                         joueurs1Score++;
-                        System.out.println("JOUEUR 1 a gagné cette manche! Score:" + joueurs1Score + "-" + joueurs2Score + "\n");
+                        Pep8.stro("JOUEUR 1 a gagné cette manche! Score:" + joueurs1Score + "-" + joueurs2Score + "\n\n");
                         counter--;
                     }else if (joueurs2Choix == 'c') {
                         joueurs2Score++;
-                        System.out.println("JOUEUR 1 a gagné cette manche! Score:" + joueurs1Score + "-" + joueurs2Score + "\n");
+                        Pep8.stro("JOUEUR 1 a gagné cette manche! Score:" + joueurs1Score + "-" + joueurs2Score + "\n\n");
                         counter--;
                     }else{
-                        System.out.println("Manche null \n");
+                        Pep8.stro("Manche null \n\n");
                     }
                     break;
             }
@@ -95,10 +95,10 @@ public class RPC {
 
         //Message final : affiche le gagnant du match et le score
         if (joueurs1Score > joueurs2Score) {
-            System.out.println("Joueur1 a gagné le match! félicitation !");
+            Pep8.stro("Joueur1 a gagné le match! félicitation !\n");
         }else{
-            System.out.println("Joueur2 a gagné le match! félicitation !");
+            Pep8.stro("Joueur2 a gagné le match! félicitation !\n");
         }
-        System.out.println("Score final " + joueurs1Score + " - " + joueurs2Score);
+        Pep8.stro("Score final " + joueurs1Score + " - " + joueurs2Score);
     }
 }
